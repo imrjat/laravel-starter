768904392300d9c45fcdcc1e6ea7a2760ec2bb8c
David Carr <dave@dcblog.dev>
2023-03-05 18:20:23 +0000

    removed sent emails

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -33,6 +34,10 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        if (config('app,env') !== 'testing') {
+            DB::statement('ALTER TABLE users ADD FULLTEXT (name, email)');
+        }
     }
 
     /**
