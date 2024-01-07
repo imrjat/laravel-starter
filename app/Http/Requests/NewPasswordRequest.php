0f2a2737cb4a0270b39a6da21128dfeabbd2776a
David Carr <dave@dcblog.dev>
2024-01-07 12:48:21 +0000

    improved test coverage

---
+++ app/Http/Requests/NewPasswordRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class NewPasswordRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'token' => [
+                'required'
+            ],
+            'email' => [
+                'required',
+                'email'
+            ],
+            'password' => [
+                'required',
+                'confirmed',
+                Password::defaults()
+            ],
+        ];
+    }
+}
