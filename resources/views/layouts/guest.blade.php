9aaf0388f38739312cbf0a0206363017f7ca5e96
David Carr <dave@dcblog.dev>
2024-01-11 17:52:57 +0000

    set page title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
