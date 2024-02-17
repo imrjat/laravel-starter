9b621219865583c21cdab84af205eba86060e1fb
David Carr <dave@dcblog.dev>
2024-02-17 16:05:25 +0000

    refactored tests to use describe

--- tests/Feature/ArchitecturalTest.php
+++ tests/Feature/ArchitecturalTest.php
@@ -2,21 +2,30 @@
 
 use App\Http\Controllers\Controller;
 
-test('globals')
-    ->expect(['dd', 'dump', 'ray', 'env'])
-    ->not->toBeUsed();
+describe('Architectural Tests', function () {
+    test('globals')
+        ->expect(['dd', 'dump', 'ray', 'env', 'var_dump', 'print_r', 'var_export', 'die'])
+        ->not->toBeUsed();
 
-test('traits')
-    ->expect('App\Models\Traits')
-    ->toBeTraits();
+    test('traits')
+        ->expect('App\Models\Traits')
+        ->toBeTraits();
 
-test('strict types')
-    ->expect('App')
-    ->toUseStrictTypes();
+    test('strict types')
+        ->expect('App')
+        ->toUseStrictTypes();
 
-test('controllers')
-    ->expect('App\Http\Controllers')
-    ->toHaveSuffix('Controller')
-    ->toBeClasses()
-    ->classes->not->toBeFinal()
-    ->classes->toExtend(Controller::class);
+    test('value objects')
+        ->expect('App\ValueObjects')
+        ->toBeReadOnly()
+        ->not->toBeFinal()
+        ->toExtendNothing()
+        ->toImplementNothing();
+
+    test('controllers')
+        ->expect('App\Http\Controllers')
+        ->toHaveSuffix('Controller')
+        ->classes->not->toBeFinal()
+        ->classes->toExtend(Controller::class)
+        ->toImplementNothing();
+});
--- tests/Feature/Http/Controllers/WelcomeTest.php
+++ tests/Feature/Http/Controllers/WelcomeTest.php
@@ -2,54 +2,60 @@
 
 use function Pest\Laravel\get;
 
-test('can see welcome page', function () {
-    get('/')->assertOk();
+get('/')->assertOk();
+
+describe('authenticated', function () {
+
+    beforeEach(function () {
+        $this->authenticate();
+    });
+
+    test('can see dashboard text on welcome page when logged in', function () {
+        $this->app['config']->set(['admintw.is_live' => true]);
+
+        get('/')->assertSeeText('Dashboard');
+    });
+
+    test('cannot see dashboard text on welcome page when logged in but not in live mode', function () {
+        $this->app['config']->set(['admintw.is_live' => false]);
+
+        get('/')->assertDontSeeText('Dashboard');
+    });
+
 });
 
-test('can see dashboard text on welcome page when logged in', function () {
-    $this->app['config']->set(['admintw.is_live' => true]);
+describe('guest', function () {
 
-    $this->authenticate()
-        ->get('/')
-        ->assertSeeText('Dashboard');
-});
+    test('can see login text on welcome page when in live mode', function () {
+        $this->app['config']->set(['admintw.is_live' => true]);
 
-test('cannot see dashboard text on welcome page when logged in but not in live mode', function () {
-    $this->app['config']->set(['admintw.is_live' => false]);
+        get('/')
+            ->assertSeeText('Login')
+            ->assertSeeText('Start free trial');
+    });
 
-    $this->authenticate()
-        ->get('/')
-        ->assertDontSeeText('Dashboard');
-});
+    test('cannot see login text on welcome page when not in live mode', function () {
+        $this->app['config']->set(['admintw.is_live' => false]);
 
-test('can see login text on welcome page when in live mode', function () {
-    $this->app['config']->set(['admintw.is_live' => true]);
+        get('/')
+            ->assertDontSeeText('Login')
+            ->assertDontSeeText('Start free trial');
+    });
 
-    get('/')
-        ->assertSeeText('Login')
-        ->assertSeeText('Start free trial');
-});
+    test('can see free trail text on welcome page when in live mode', function () {
+        $this->app['config']->set(['admintw.is_live' => true]);
 
-test('cannot see login text on welcome page when not in live mode', function () {
-    $this->app['config']->set(['admintw.is_live' => false]);
+        get('/')
+            ->assertSeeText('free '.config('admintw.trail_days').' days trial')
+            ->assertSeeText('Start your free '.config('admintw.trail_days').' days trial');
+    });
 
-    get('/')
-        ->assertDontSeeText('Login')
-        ->assertDontSeeText('Start free trial');
-});
+    test('cannot see free trail text on welcome page when not in live mode', function () {
+        $this->app['config']->set(['admintw.is_live' => false]);
 
-test('can see free trail text on welcome page when in live mode', function () {
-    $this->app['config']->set(['admintw.is_live' => true]);
+        get('/')
+            ->assertDontSeeText('Start free '.config('admintw.trail_days').' days trial')
+            ->assertDontSeeText('Start your free '.config('admintw.trail_days').' days trial');
+    });
 
-    get('/')
-        ->assertSeeText('free '.config('admintw.trail_days').' days trial')
-        ->assertSeeText('Start your free '.config('admintw.trail_days').' days trial');
-});
-
-test('cannot see free trail text on welcome page when not in live mode', function () {
-    $this->app['config']->set(['admintw.is_live' => false]);
-
-    get('/')
-        ->assertDontSeeText('Start free '.config('admintw.trail_days').' days trial')
-        ->assertDontSeeText('Start your free '.config('admintw.trail_days').' days trial');
 });
