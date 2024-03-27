b560f115300acad8c7a9741ed5a0026551554a80
Shift <shift@laravelshift.com>
2024-03-27 15:45:22 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,23 +7,5 @@ return [
 
     'user_agent' => env('USER_AGENT', ''),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
 ];
