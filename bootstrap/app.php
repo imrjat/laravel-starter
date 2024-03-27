bd8a59f34d9722c2af9ba81509147cf5e6a467fa
Shift <shift@laravelshift.com>
2024-03-27 15:45:21 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,27 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(fn () => route('dashboard'));
+
+        $middleware->throttleApi();
+
+        $middleware->group('for_livewire', [
+            \App\Http\Middleware\RolePermissions::class,
+        ]);
+
+        $middleware->replaceInGroup('web', \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class, \App\Http\Middleware\VerifyCsrfToken::class);
+
+        $middleware->alias([
+            'IpCheckMiddleware' => \App\Http\Middleware\IpCheckMiddleware::class,
+            'RedirectIfNotSubscribed' => \App\Http\Middleware\RedirectIfNotSubscribed::class,
+            'activeUser' => \App\Http\Middleware\ActiveUser::class,
+            'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+            'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+            'signed' => \App\Http\Middleware\ValidateSignature::class,
+            'tenantOwner' => \App\Http\Middleware\RedirectIfNotOwner::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
